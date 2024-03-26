0361714762441d19a79a0039f720cc39ccc67dc1
azmain-siam <siamhossain2700@gmail.com>
2024-03-26 14:22:08 +0600

    Books Card added

--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -8,7 +8,7 @@ const Books = () => {
       <h3 className="playfair text-4xl font-bold text-[#131313] text-center mt-24 mb-10">
         Books
       </h3>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
         {books.map((book) => (
           <BooksCard key={book.id} book={book}></BooksCard>
         ))}
--- src/Components/BooksCard.jsx
+++ src/Components/BooksCard.jsx
@@ -1,22 +1,43 @@
+import starIcon from "../assets/Vector.svg";
+
 const BooksCard = ({ book }) => {
   console.log(book);
   const { rating, category, tags, image, bookName, author } = book;
 
   return (
     <div>
-      <div className="card bg-base-100 shadow-xl ">
-        <figure className="px-10 pt-10 h-[250px]">
+      <div className="card p-6 border h-full border-[#28282833] work-sans cursor-pointer hover:border-[#23BE0A] hover:scale-[1.03] transition-all duration-200 hover:bg-[#22be0a05]">
+        <figure className="h-[220px]">
           <img
             src={image}
             alt="Shoes"
-            className="rounded-xl h-full object-cover"
+            className="rounded-xl h-full object-cover object-center"
           />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{bookName}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card-body p-0 pt-6">
+          <div className="flex gap-3">
+            {tags.map((tag) => (
+              <span
+                key={tag}
+                className="px-3 py-2 bg-[#23BE0A0D] text-[#23BE0A] text-sm"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+          <h2 className="card-title mb-3 playfair text-2xl font-bold">
+            {bookName}
+          </h2>
+          <p className="font-medium text-[#150B2B99]">By: {author}</p>
+          <hr className="my-1 border-dashed" />
+          <div className="flex items-center justify-between gap-5 mb-2 text-[#131313CC] font-medium">
+            <div className="flex items-center gap-2 md:gap-3">
+              <p className="text-sm md:text-base">{category}</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <p className="text-sm md:text-base">{rating}</p>
+              <img className="w-4" src={starIcon} />
+            </div>
           </div>
         </div>
       </div>
